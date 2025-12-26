d271c63ccfbe63c485f745278cdca57364ae8598
Lurzard <lurienthelizard@gmail.com>
2025-12-26 13:09:30 -0500

    minor change to thanatosis sequence for now

--- src/CWTs/Cycle/ThanatosisTutorialSequence.cs
+++ src/CWTs/Cycle/ThanatosisTutorialSequence.cs
@@ -417,6 +417,10 @@ public class ThanatosisTutorialSequence
         }
 
         float fate = demise - fight;
+        if (fate >= 0.8)
+        {
+            fate = 0.75f;
+        }
         targetDeathIntensity = fate;
     }
 
