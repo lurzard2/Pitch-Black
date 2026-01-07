80c357604461669750e0cc2dabb92d7b2bf04380
Lurzard <lurienthelizard@gmail.com>
2026-01-07 01:59:28 -0500

    starting the hud meter

---
+++ src/Objects/HUD/CycleMeter.cs
@@ -0,0 +1,17 @@
+﻿using HUD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HUD.HUD;
+
+namespace PitchBlack;
+
+public class CycleMeter : HudPart
+{
+
+    public CycleMeter(HUD.HUD hud) : base(hud)
+    {
+    }
+}
