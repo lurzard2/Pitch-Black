359e06f8db64c3b9e0eef872afc7cf07d2c22fd6
Lurzard <lurienthelizard@gmail.com>
2026-03-23 03:29:15 -0400

    ripplepoint wip

---
+++ src/Dimensions/RipplePoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Text;
+using System.Threading.Tasks;
+using Watcher;
+
+namespace PitchBlack.Dimensions
+{
+    public struct RipplePointData
+    {
+        public string roomName;
+        public Vector2 screenPos;
+        public float radius;
+        public float intensity;
+    }
+
+    public class RipplePoint : UpdatableAndDeletable
+    {
+        public RipplePointData data;
+
+        public RipplePoint(Room room, RipplePointData pointData)
+        {
+            data = pointData;
+        }
+
+        public override void Update(bool eu)
+        {
+        }
+
+        public override void Destroy()
+        {
+        }
+    }
+}
